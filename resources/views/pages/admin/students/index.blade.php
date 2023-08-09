afc4be63b9d702218eac2b83b174d5c29d36e052
Fazril Arief Nugraha <fazrilarief331@gmail.com>
2023-08-09 11:44:41 +0700

    first commit

---
+++ resources/views/pages/admin/students/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.admin')
+
+@section('content')
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Data Siswa</h1>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                    class="fas fa-download fa-sm text-white-50"></i>Tambah Data Siswa</a>
+        </div>
+
+        <div class="row-12">
+            <div class="card-shadow">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered text-center">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Nama</th>
+                                    <th>NIS</th>
+                                    <th>Email</th>
+                                    <th>Tempat Lahir</th>
+                                    <th>Taggal Lahir</th>
+                                    <th>Jenis Kelamin</th>
+                                    <th>Angkatan</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($students as $student)
+                                    <tr>
+                                        <td>{{ $student->nama }}</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+@endsection
